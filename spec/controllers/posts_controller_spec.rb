f5c2ab44c8ca2d1022bceeac5131c8f914090174
Kevin F Browne <kbrowneatx@gmail.com>
2013-06-11 19:19:11 -0500

    playing around to see what works

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe PostsController do
 	describe new do
 		it "requires a name and content" do
-			p = Post.new(title:"This is Title text.", content:"This is the content text.", user_id: 1).should be_an_instance_of Post
+			p = Post.new
+			p.title = "This is Title text."
+			p.content = "This is the content text."
+			p.user_id = 1
+			p.should be_an_instance_of Post
 		end
 		
 		it "requires a user_id" do
