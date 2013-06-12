f994dca80fe2322c5dd25365f9c8f9e90aa568e8
Kevin F Browne <kbrowneatx@gmail.com>
2013-06-11 21:54:59 -0500

    postscontroller tests work

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,23 +1,35 @@
 require 'spec_helper'
 
 describe PostsController do
+	before :each do
+		@post1 = Post.create!(title: 'This is the title', content: 'This is the body text')
+		@post2 = Post.create!(title: 'This is the title', content: 'This is the body text')
+		@user1 = User.create!(name:'Joe', email:'joe@foo.net', password: 'password', password_confirmation: 'password', bio:'this is my life', role:'admin')
+	end
   describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :index
-      expect(response).to be_success
-      expect(response.status).to eq(200)
-    end
-
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-
     it "loads all of the posts into @posts" do
-      post1, post2 = Post.create!, Post.create!
       get :index
+      expect(assigns(:posts)).to match_array([@post1, @post2])
+    end
+  end
+  describe "PUT #update" do
+    it "all post attributes are mass assignable" do
+      @post1.title = 'New Title'
+      @post1.content = 'New body text for content'
+      @post1.user_id = @user1.id
+			
+      @post1.title.should eql('New Title')
+      @post1.content.should eql('New body text for content')
+      @post1.user_id.should eql(@user1.id)
+    end
 
-      expect(assigns(:posts)).to match_array([post1, post2])
+    it "posts accept new comments" do
+      comment1 = @post1.comments.create
+			comment1.content = 'New body text for comment'
+      comment1.user_id = @user1.id
+			
+      @post1.comments.last.content.should eql('New body text for comment')
+      @post1.comments.last.user_id.should eql(@user1.id)
     end
   end
 end
